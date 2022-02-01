2c947dee157e521fc8b712c797bd435deac81606
Alex Markley <alex@alexmarkley.com>
2022-01-31 20:58:35 -0500

    start updating plugin for blender 2.93 LTS; update the setup script in anticipation of a CI job

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+set -e
+set -o pipefail
+
+BASEPATH="$( cd "$(dirname "${0}")" ; pwd -P )"
+cd "${BASEPATH}"
+
+rm -rf vendor
+mkdir vendor
+
+export PYTHONDONTWRITEBYTECODE=1
 pip3 install --upgrade -r requirements.txt --target vendor/ --no-binary :all:
-
-
-
